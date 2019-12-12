f3a98cfda6f938a9f3dc18035568a7f7ee423393
muneebGH <muneeb.github@gmail.com>
2019-12-12 10:11:42 +0500

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Muneeb Ur Rehman on 12/12/2019.
+//  Copyright © 2019 Muneeb Ur Rehman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
